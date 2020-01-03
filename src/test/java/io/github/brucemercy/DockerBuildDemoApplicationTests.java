4175dc4ede27e483173fbb19634b4b8da9c87357
mildchary <email.chengcoder@gmail.com>
2020-01-03 15:08:25 +0800

    Add files via upload

---
+++ src/test/java/io/github/brucemercy/DockerBuildDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package io.github.brucemercy;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DockerBuildDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
